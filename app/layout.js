2aa3917bb671686bb6a39b6d71bcf3b07c58d952
Aditya Pratap Singh <2004adityaprataps@gmail.com>
2024-03-29 03:57:41 +0530

    styles changed

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,6 @@
-import { Inter } from "next/font/google";
-import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+import "../styles/globals.css";
+
 
 export const metadata = {
   title: "Create Next App",
--- app/page.js
+++ app/page.js
@@ -6,7 +6,7 @@ import ProductList from "@/Components/ProductList";
 export default function Home() {
   return (
     
-    <div className="">
+    <div >
       <Hero/>
       <ProductList/>
     </div>
